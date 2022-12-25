d2a3fbb1c1b6d40a58428e506e675a0b7a9f5e21
GhosTHaise <Ghostrex2@gmail.com>
2022-12-25 21:39:07 +0300

    Build dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
 use structopt::StructOpt;
-
-#[derive(StructOpt)]
+#[derive(StructOpt)] 
+//use std::env;
 struct Cli {
+    city : String,
+    counrty_code  : String
+}
 
-}
 fn main() {
-    println!("Hello, world!");
-}
+    let args = Cli::from_args();
+    //let args : Vec<String> = env::args().collect();
+    //println!("Mes args : {:?}",args[1]);
+    
+    println!("Our city : {} , our country code : {}",args.city,args.country_code);
